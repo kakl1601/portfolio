db2af3364dc1fe9859c1a9356c81b4207a741a49
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2018-04-30 13:33:51 +0200

    Content Styling

--- my-app/src/components/FirstPage.js
+++ my-app/src/components/FirstPage.js
@@ -4,7 +4,8 @@ class FirstPage extends React.Component {
  render() {
    return (
      <div>
-       <h1>KANYA</h1>
+        <h3 className="text-center">I AM</h3>
+        <h1 className="text-center">KANYARAT</h1>
      </div>
    );
  }
