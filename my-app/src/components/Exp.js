63517dd6d38893db24c8b2ca6987b3b031f0252f
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2018-05-03 00:46:38 +0200

    exp-component styling

--- my-app/src/components/Exp.js
+++ my-app/src/components/Exp.js
@@ -2,13 +2,32 @@ import React, { Component } from "react";
 import {Container, Col, Row} from "reactstrap";
 
 class Exp extends React.Component {
- render() {
-   return (
-     <div>
-       <h1>Tidigare Projekt</h1>
-     </div>
-   );
- }
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          <h2>TIDIGARE PROJEKT</h2>
+        </Row>
+        <Row className="mt-5">
+          <Col>
+            <Row className="justify-content-center">
+              <h5>TIGER LMS</h5>
+            </Row>
+          </Col>
+          <Col>
+            <Row className="justify-content-center">
+              <h5>CATCH COINS</h5>
+            </Row>
+          </Col>
+          <Col>
+            <Row className="justify-content-center">
+              <h5>EYEODA</h5>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Exp;
