7234714fb351712108db80577b5b1f0132804597
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2018-05-03 00:20:11 +0200

    Add percentage circle in knowledge-component

--- my-app/src/components/Knowledge.js
+++ my-app/src/components/Knowledge.js
@@ -1,21 +1,78 @@
 import React, { Component } from "react";
+import {Container, Col, Row} from "reactstrap";
+
 
 class Knowledge extends Component {
- render() {
-   return (
-     <div>
-       <h2>Färdigheter</h2>
-       <p>Mauris sem velit, vehicula eget sodales vitae,
-       rhoncus eget sapien:</p>
-       <ol>
-         <li>Nulla pulvinar diam</li>
-         <li>Facilisis bibendum</li>
-         <li>Vestibulum vulputate</li>
-         <li>Eget erat</li>
-         <li>Id porttitor</li>
-       </ol>
-     </div>
-   );
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          <h2>FÄRDIGHETER</h2>
+        </Row>
+        <Row className="justify-content-center mt-5">
+          <Col>
+          <h4 className="text-center">HTML5</h4>
+          <div className="c100 p90 medium mt-4">
+            <span>90%</span>
+            <div className="slice">
+              <div className="bar"></div>
+              <div className="fill"></div>
+            </div>
+          </div>
+          </Col>
+          <Col>
+            <h4 className="text-center">CSS3</h4>
+            <div className="c100 p80 medium mt-4">
+              <span>80%</span>
+              <div className="slice">
+                <div className="bar"></div>
+                <div className="fill"></div>
+              </div>
+            </div>
+          </Col>
+          <Col className="justify-content-center">
+            <h4 className="text-center">Javascript</h4>
+            <div className="c100 p60 medium mt-4">
+              <span>60%</span>
+              <div className="slice">
+                <div className="bar"></div>
+                <div className="fill"></div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <h4 className="text-center">React</h4>
+            <div className="c100 p65 medium mt-4">
+              <span>65%</span>
+              <div className="slice">
+                <div className="bar"></div>
+                <div className="fill"></div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <h4 className="text-center">PHP</h4>
+            <div className="c100 p50 medium mt-4">
+              <span>50%</span>
+              <div className="slice">
+                <div className="bar"></div>
+                <div className="fill"></div>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <h4 className="text-center">MYSQL</h4>
+            <div className="c100 p50 medium mt-4">
+              <span>50%</span>
+              <div className="slice">
+                <div className="bar"></div>
+                <div className="fill"></div>
+              </div>
+            </div>
+          </Col>
+      </Row>
+    </Container>
+  );
  }
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './circle.css';
 import App from './components/App';
 import Main from './components/Main';
 import registerServiceWorker from './registerServiceWorker';
