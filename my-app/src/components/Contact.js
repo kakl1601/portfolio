9794cd6424fb7b9552eee9f2ce0c978b0b5aadf2
kakl1601 <32056639+kakl1601@users.noreply.github.com>
2018-05-06 16:49:22 +0200

    Small fixes

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -19,15 +19,15 @@ class Contact extends React.Component {
                         <div className="text-center">
                             <h6>KANYARAT KLAYJINDA</h6>
                             <p>072 002 5016</p>
-                            <h6>kanyarat0907@hotmail.com</h6>
-                            <div className="social mt-5">
+                            <a href="mailto:kanyarat0709@hotmail.com" className="none-dec">kanyarat0907@hotmail.com</a>
+                            <Container className="social mt-5">
                                 <h4>FÖLJ MIG</h4> 
                                 <Row className="mt-4">  
-                                <Col><a className="social-icon" href="https://www.facebook.com/kaewaiize"><img src="http://simpleicon.com/dev/wp-content/uploads/facebook-2-256x256.png" className="social-icon" /></a></Col>
+                                <Col><a className="social-icon" href="https://www.facebook.com/kaewaiize"><img src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_circle_black-128.png" className="social-icon" /></a></Col>
                                 <Col><a className="social-icon" href="https://twitter.com/Kaewaiize"><img src="https://www.shareicon.net/download/2015/08/04/79820_circle_512x512.png" className="social-icon"/></a></Col>
                                 <Col><a className="social-icon" href="https://www.linkedin.com/in/kanyarat-klayjinda-5744b7bb"><img src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_black-512.png" className="social-icon"/></a></Col>
                                 </Row>
-                            </div>  
+                            </Container>  
                         </div>
                     </Row>
                 </Col>
